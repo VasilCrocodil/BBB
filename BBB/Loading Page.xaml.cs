3974a55eff1206cf5f8b9614f7d0b29872645eaf
v.n.georgiev23 <v.n.georgiev23@LABSCIFIPC11>
2023-02-09 09:40:54 +0200

    Add project files.

---
+++ BBB/Loading Page.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace BBB
+{
+    /// <summary>
+    /// Interaction logic for Loading_Page.xaml
+    /// </summary>
+    public partial class Loading_Page : Window
+    {
+        public Loading_Page()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection sqlCon = new SqlConnection(@"Data Source=LABSCIFIPC11\LOCALHOST; Initial Catalog =Vasil_Login; Integrated Security = True ");
+
+
+            try
+            {
+                if (sqlCon.State == ConnectionState.Closed)
+                    sqlCon.Open();
+
+                string query = "SELECT COUNT(1) FROM User Where Username=@Username and Password=@Password";
+                SqlCommand sqlCommand = new SqlCommand(query, sqlCon);
+                sqlCommand.CommandType = CommandType.Text;
+                sqlCommand.Parameters.AddWithValue("@Username", txtUsername.Text);
+                sqlCommand.Parameters.AddWithValue("@Password", txtPassword.Password);
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }   
+}
